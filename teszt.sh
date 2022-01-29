b4d303380a3a3edd2c9e57870c9c1fa49bc6ffcc
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2022-01-29 10:44:56 +0100

    Correction to OpenMP
    Unfortunately wrong script exaple was copied. Corrections:
    - Estimated run decreased
    - SLURM parameter corrected, now not OpenMPI is the manager.

--- teszt.sh
+++ teszt.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=EMAIL
 #SBATCH --job-name=teszt
 #SBATCH -n 8
-#SBATCH --time=01:00
+#SBATCH --time=00:05
 #SBATCH -o slurm.out
 /opt/nce/bin/numainfo
-mpirun $SLURM_NTASKS ./teszt.bin
+OMP_NUM_THREADS=$SLURM_NTASKS ./teszt.bin
